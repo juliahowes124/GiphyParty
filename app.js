a41a9008fb1156507c907a33f1ab23cfbe67e301
Ben Jacobson <jacobson.ben11@gmail.com>
2021-02-23 18:47:27 -0500

    get gif objects

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Let's get this party started!");
+
+$("#form").on('submit', async function (evt) {
+    evt.preventDefault();
+    let searchTerm = $('#search').val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(response);
+})
+
