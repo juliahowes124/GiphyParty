7d39ae5ec9b4433f3ea51eeabbfaf8e8aa4e4c72
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-02-23 16:45:06 -0800

    choose random gif from response and append image, delete all button

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ $("#form").on('submit', async function (evt) {
     evt.preventDefault();
     let searchTerm = $('#search').val();
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    console.log(response);
+    let newGifUrl = chooseRandomGif(response);
+    let img = createImage(newGifUrl, searchTerm);
+    $('.container').append(img);
 })
 
+function chooseRandomGif(res) {
+    let randInt = Math.floor(Math.random() * res.data.data.length);
+    let newGif = res.data.data[randInt].images.original.url;
+    return newGif;
+}
+
+function createImage(url, searchTerm) {
+    return `<img src="${url}" alt="${searchTerm}">` //search term for alt
+}
+
+$("#delete").on("click", function () {
+    $('.container').empty();
+})
