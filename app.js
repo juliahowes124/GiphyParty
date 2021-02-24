9dc5ed417963c662b8542441a8c1ce54a3a6a949
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-02-23 21:23:50 -0800

    simple styling

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log("Let's get this party started!");
-
-$("#form").on('submit', async function (evt) {
+$("#search_btn").on('click', async function (evt) {
     evt.preventDefault();
     let searchTerm = $('#search').val();
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
